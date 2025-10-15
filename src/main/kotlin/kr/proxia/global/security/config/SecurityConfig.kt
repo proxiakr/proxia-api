b3f0244e14b8b3e56d0796d561fecfe79898fdb4
jbj338033 <jbj338033@gmail.com>
2025-10-16 01:52:01 +0900

    feat: add base security config

---
+++ src/main/kotlin/kr/proxia/global/security/config/SecurityConfig.kt
@@ -0,0 +1,32 @@
+package kr.proxia.global.security.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain = http
+        .csrf { it.disable() }
+        .cors { it.configurationSource(corsConfigurationSource()) }
+
+        .authorizeHttpRequests { it
+            .anyRequest().permitAll()
+        }
+
+        .build()
+
+    private fun corsConfigurationSource() = UrlBasedCorsConfigurationSource().apply {
+        registerCorsConfiguration("/**", CorsConfiguration().apply {
+            allowedOriginPatterns = listOf("*")
+            allowedMethods = listOf("*")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+            maxAge = 3600L
+        })
+    }
+}
