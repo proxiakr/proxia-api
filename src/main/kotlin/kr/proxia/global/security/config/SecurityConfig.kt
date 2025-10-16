2097161b0e575d40a899699aa4a6b5fd45bfc2b2
jbj338033 <jbj338033@gmail.com>
2025-10-16 20:58:24 +0900

    feat: add security config

--- src/main/kotlin/kr/proxia/global/security/config/SecurityConfig.kt
+++ src/main/kotlin/kr/proxia/global/security/config/SecurityConfig.kt
@@ -3,12 +3,17 @@ package kr.proxia.global.security.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class SecurityConfig {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain = http
         .csrf { it.disable() }
