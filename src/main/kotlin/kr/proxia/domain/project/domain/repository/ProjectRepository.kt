74745de1196892b7a0925a3de5df5f0141fd51c9
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-10-20 00:57:40 +0900

    feat: add project repository

---
+++ src/main/kotlin/kr/proxia/domain/project/domain/repository/ProjectRepository.kt
@@ -0,0 +1,9 @@
+package kr.proxia.domain.project.domain.repository
+
+import kr.proxia.domain.project.domain.entity.ProjectEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProjectRepository : JpaRepository<ProjectEntity, Long> {
+}
