ab68337826ceb60d3c4eef7e38c1426b28347459
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-10-20 00:03:55 +0900

    feat: add annotation

--- src/main/kotlin/kr/proxia/domain/project/domain/entity/ProjectEntity.kt
+++ src/main/kotlin/kr/proxia/domain/project/domain/entity/ProjectEntity.kt
@@ -1,9 +1,11 @@
 package kr.proxia.domain.project.domain.entity
 
 import jakarta.persistence.Entity
+import jakarta.persistence.Table
 import kr.proxia.global.jpa.common.BaseEntity
 
 @Entity
+@Table(name = "projects")
 class ProjectEntity(
     val userId: Long,
     val gitIntegrationId: Long,
