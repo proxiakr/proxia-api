cc9169bce7f6990dcbd01372910cd379387d475b
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-10-25 00:59:14 +0900

    refactor: add trailing comma

--- src/main/kotlin/kr/proxia/domain/project/presentation/v1/controller/ProjectController.kt
+++ src/main/kotlin/kr/proxia/domain/project/presentation/v1/controller/ProjectController.kt
@@ -3,22 +3,27 @@ package kr.proxia.domain.project.presentation.v1.controller
 import kr.proxia.domain.project.application.service.ProjectService
 import kr.proxia.domain.project.presentation.v1.docs.ProjectDocs
 import kr.proxia.domain.project.presentation.v1.request.CreateProjectRequest
+import kr.proxia.global.response.OffsetLimit
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/projects")
-class ProjectController(private val projectService: ProjectService) : ProjectDocs {
+class ProjectController(
+    private val projectService: ProjectService,
+) : ProjectDocs {
     @PostMapping
     override fun createProject(@RequestBody request: CreateProjectRequest) = projectService.createProject(request)
 
     @GetMapping
-    override fun getProjects() = projectService.getProjects()
+    override fun getProjects(@RequestParam offset: Long, @RequestParam limit: Int) =
+        projectService.getProjects(OffsetLimit(offset, limit))
 
     @GetMapping("/{projectId}")
     override fun getProject(@PathVariable projectId: Long) = projectService.getProject(projectId)
