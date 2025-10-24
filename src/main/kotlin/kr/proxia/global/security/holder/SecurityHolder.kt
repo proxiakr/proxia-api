cbd799fa7030dec94caa6af559830f1b789c4f79
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-10-25 01:00:18 +0900

    refactor: change format

--- src/main/kotlin/kr/proxia/global/security/holder/SecurityHolder.kt
+++ src/main/kotlin/kr/proxia/global/security/holder/SecurityHolder.kt
@@ -6,10 +6,18 @@ import org.springframework.stereotype.Component
 @Component
 class SecurityHolder {
     fun getUserId(): Long {
-        val principal = SecurityContextHolder.getContext().authentication.principal ?: throw IllegalStateException("Authentication principal is null")
+        val principal = SecurityContextHolder.getContext().authentication.principal
+            ?: throw IllegalStateException("Authentication principal is null")
 
         return principal as Long
     }
 
     fun isAuthenticated() = SecurityContextHolder.getContext().authentication.isAuthenticated
+//
+//    val userId: Long
+//        get() = SecurityContextHolder.getContext().authentication.principal?.let { it as? Long }
+//            ?: throw IllegalStateException("Authentication principal is null")
+//
+//    val isAuthenticated: Boolean
+//        get() = SecurityContextHolder.getContext().authentication.isAuthenticated
 }
