ea10a6ab4ebce7d32104fdc95f3f443edb77e237
jbj338033 <jbj338033@gmail.com>
2025-11-10 15:52:11 +0900

    fix: add encryption and optimize N+1 queries

--- src/main/kotlin/kr/proxia/domain/service/presentation/request/UpdateServiceRequest.kt
+++ src/main/kotlin/kr/proxia/domain/service/presentation/request/UpdateServiceRequest.kt
@@ -1,9 +1,13 @@
 package kr.proxia.domain.service.presentation.request
 
+import kr.proxia.domain.resource.presentation.request.CreateAppResourceRequest
+import kr.proxia.domain.resource.presentation.request.CreateDatabaseResourceRequest
 import kr.proxia.domain.service.domain.enums.ServiceType
 
 data class UpdateServiceRequest(
     val name: String,
     val description: String?,
     val type: ServiceType,
+    val appResource: CreateAppResourceRequest?,
+    val databaseResource: CreateDatabaseResourceRequest?,
 )
