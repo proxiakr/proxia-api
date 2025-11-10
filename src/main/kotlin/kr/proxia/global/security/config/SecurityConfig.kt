1322c36e0bce559c26c0f90d7022d713510befaf
jbj338033 <jbj338033@gmail.com>
2025-11-10 17:31:25 +0900

    style: apply ktlint formatting

--- src/main/kotlin/kr/proxia/global/security/config/SecurityConfig.kt
+++ src/main/kotlin/kr/proxia/global/security/config/SecurityConfig.kt
@@ -26,11 +26,11 @@ class SecurityConfig(
             .cors { it.configurationSource(corsConfigurationSource()) }
             .headers { headers ->
                 headers.frameOptions { it.sameOrigin() }
-            }
-            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            }.sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .authorizeHttpRequests {
                 it
-                    .requestMatchers("/h2-console/**").permitAll()
+                    .requestMatchers("/h2-console/**")
+                    .permitAll()
                     .anyRequest()
                     .permitAll()
             }.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
