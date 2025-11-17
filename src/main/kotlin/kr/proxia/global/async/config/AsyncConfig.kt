8a5d779baba8ba7eddd88f9ac47b3bdf48449240
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2025-11-17 16:40:24 +0900

    feat: add async config

---
+++ src/main/kotlin/kr/proxia/global/async/config/AsyncConfig.kt
@@ -0,0 +1,8 @@
+package kr.proxia.global.async.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@Configuration
+@EnableAsync
+class AsyncConfig
