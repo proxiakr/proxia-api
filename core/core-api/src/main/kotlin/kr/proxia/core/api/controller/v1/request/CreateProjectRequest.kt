ccadeab3a1fb6412ca3f6fb8c74477f8585a55e2
jbj338033 <jbj338033@gmail.com>
2026-02-06 08:50:00 +0900

    feat: add validation to create request

--- core/core-api/src/main/kotlin/kr/proxia/core/api/controller/v1/request/CreateProjectRequest.kt
+++ core/core-api/src/main/kotlin/kr/proxia/core/api/controller/v1/request/CreateProjectRequest.kt
@@ -1,9 +1,17 @@
 package kr.proxia.core.api.controller.v1.request
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 import kr.proxia.core.domain.CreateProject
 
 data class CreateProjectRequest(
+    @field:NotBlank
     val name: String,
+    @field:NotBlank
+    @field:Pattern(
+        regexp = "^[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?$",
+        message = "Subdomain must start and end with a lowercase letter or digit, and can contain hyphens in between. Length must be between 1 and 63 characters.",
+    )
     val subdomain: String,
 ) {
     fun toDomain() =
