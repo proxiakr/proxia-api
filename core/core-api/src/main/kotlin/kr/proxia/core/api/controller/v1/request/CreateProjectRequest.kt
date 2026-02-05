e10f93572474267d513d1079ad77532cdd521546
jbj338033 <jbj338033@gmail.com>
2026-02-06 08:57:38 +0900

    style: apply ktlint format

--- core/core-api/src/main/kotlin/kr/proxia/core/api/controller/v1/request/CreateProjectRequest.kt
+++ core/core-api/src/main/kotlin/kr/proxia/core/api/controller/v1/request/CreateProjectRequest.kt
@@ -10,7 +10,9 @@ data class CreateProjectRequest(
     @field:NotBlank
     @field:Pattern(
         regexp = "^[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?$",
-        message = "Subdomain must start and end with a lowercase letter or digit, and can contain hyphens in between. Length must be between 1 and 63 characters.",
+        message =
+            "Subdomain must start and end with a lowercase letter or digit, " +
+                "and can contain hyphens in between. Length must be between 1 and 63 characters.",
     )
     val subdomain: String,
 ) {
